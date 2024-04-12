52188e50bbaf25caf3eae361074e0210b41bc6b3
UnknownSuperficialNight <88142731+UnknownSuperficialNight@users.noreply.github.com>
2024-04-12 13:04:42 +1200

    Fixed Math to convert Millijoule to Kilowatt-hour

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,15 @@ fn main() {
     let total_energy_millijoules = device.total_energy_consumption().unwrap();
 
     // Convert total energy consumption from millijoules to kilowatt-hours
-    let total_energy_kwh = total_energy_millijoules / 3_600_000;
+    let total_energy_kwh = total_energy_millijoules / 3_600_000_000;
 
     print!("\x1B[2J\x1B[1;1H");
 
     println!("Total Energy Consumption: {} kWh", total_energy_kwh);
 
     println!("GPU_name: {:?}", device.name().unwrap());
-    println!("fan_speed: {:?}", device.fan_speed(0).unwrap());
     println!("num_fans: {:?}", device.num_fans().unwrap());
+    println!("fan_speed: {:?}", device.fan_speed(0).unwrap());
     let memory_info = device.memory_info().unwrap();
     let free_memory_gib = memory_info.free as f64 / 1073741824.0;
     let total_memory_gib = memory_info.total as f64 / 1073741824.0;
